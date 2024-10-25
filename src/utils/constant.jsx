0e96236756a27cb5de2fd1dfd9adaab49478ead2
Ravi Kant <ravikantas.kant81@gmail.com>
2024-10-25 21:26:17 +0530

    switch local

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -1,3 +1,3 @@
 
-// export const BASE_URL = 'http://localhost:8080/';
-export const BASE_URL = 'https://node-js-dev-tinder.onrender.com/';
+export const BASE_URL = 'http://localhost:8080/';
+// export const BASE_URL = 'https://node-js-dev-tinder.onrender.com/';
